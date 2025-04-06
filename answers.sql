2d7f6febd15e54f16836b556c86cff367811d2e9
Sam <smumo4751@gmail.com>
2025-04-06 12:46:30 +0300

    very naise

---
+++ answers.sql
@@ -0,0 +1,12 @@
+-- creating a database 
+CREATE DATABASE salesDB;
+
+-- dropping database
+
+DROP DATABASE demo;
+
+
+-- verfication commands 
+
+SHOW DATABASES; -- Lists all databases
+USE salesDB;    -- Switches to salesDB (after creation)
